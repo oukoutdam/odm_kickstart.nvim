4c14a4ada00e660f6d64516ba1092ddf0490395c
Ouk Outdam <odm-labbox@OuknoMacBook-Pro.local>
2024-04-10 18:46:56 +0900

    Install nvim-ts-autotag Plugin

---
+++ lua/custom/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,10 @@
+-- File: lua/custom/plugins/nvim-ts-autotag.lua
+
+return {
+  'windwp/nvim-ts-autotag',
+  lazy = false,
+  config = {},
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
